a46bd8e9aef7b19c0f610755cc5085939f4b458c
Glahatte <55100132+Glahatte@users.noreply.github.com>
2019-09-11 16:06:39 -0400

    Add files via upload
    Wasnt able to get to the other exercise. Got stuck on the loops

---
+++ JavaFunctionTestOne.js
@@ -0,0 +1,4 @@
+var x = functionOne(4,2);
+
+function functionOne(a,b) {return a*b;
+}
---
+++ JavaLoopTest.js
@@ -0,0 +1,15 @@
+var x=2;
+var y=x+5;
+console.log (y);
+console.log(y*2);
+//I think the modulo for 14 is going to be 2
+console.log((y*2)%3);
+// I was right woo.
+var z=2;
+console.log(2%2);
+// I got the answer 0. Since there is nothing remaining it was expected
+if ((2%2)==0) {console.log("Perfeclty Balanced as all things should be");
+
+} else{console.log("Reality is often dissapointing")
+}
+if ((x+4)<100) {console.log("Aint no number high enough")
