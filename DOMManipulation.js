e163dcef609c97fedbbd9b5c893d73f473afefa5
Glahatte <55100132+Glahatte@users.noreply.github.com>
2019-09-18 15:27:01 -0400

    Add files via upload

---
+++ DOMManipulation.js
@@ -0,0 +1,10 @@
+document.getElementById ("h1").style.color="green";
+document.getElementById("ol").style.color="orange"
+document.getElementById("ol").style.fontSize="40px";
+document.getElementById("second").style.color="blue";
+
+
+  document.getElementById("second").innerHTML="Dormomu i've come to bargin";
+  document.getElementById("h1").innerHTML="You cannot hope to defeat me"
+  document.getElementById("second").className="The Bargin";
+
